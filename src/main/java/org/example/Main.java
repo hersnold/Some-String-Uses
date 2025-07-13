a70b6ed96ae4e24c8eb64c473652af932b5bbee1
hersnold <paulooliv03@gmail.com>
2025-07-13 05:19:26 -0300

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,26 @@
 package org.example;
 
+import java.util.Locale;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String original = "abcde FGHIJ ABC abc DEFG        ";
+        String lowerCase = original.toLowerCase();
+        String upperCase = original.toUpperCase(Locale.ROOT);
+        String trimCase = original.trim();
+        String substringCase = original.substring(3);
+        String substringCaseArgs = original.substring(3,9);
+        String replaceCase = original.replace('a' , 'z');
+        String replaceCaseArgs = original.replace("abc" , "zo");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(lowerCase+ "-");
+        System.out.println(upperCase + "-");
+        System.out.println(trimCase + "-");
+        System.out.println(substringCase);
+        System.out.println(substringCaseArgs);
+        System.out.println(replaceCase);
+        System.out.println(replaceCaseArgs);
     }
 }
